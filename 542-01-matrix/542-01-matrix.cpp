1e5c5f119cd193bc1f6c00459383495c068b5d31
abhi-1407 <76861961+abhi-1407@users.noreply.github.com>
2022-08-03 15:39:03 +0530

    Time: 131 ms (47.13%), Space: 30.4 MB (38.86%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -1,6 +1,6 @@
 class Solution {
 private:
-    bool checkValid(int i,int j,int m,int n)
+    bool checkValid(int i,int j,int m,int n)//to check if that index exists even
     {
         if(i<0 || j<0 || j>n-1 || i> m-1)return false;
         return true;
@@ -15,22 +15,22 @@ public:
             {
                 if(mat[i][j]==0)
                 {
-                    q.push({i,j});
-                    ans[i][j]=0;
+                    q.push({i,j});//if the element is zero then push it into the queue
+                    ans[i][j]=0;//and distance will be 0 
                 }
                 
             }
         }
         int m=mat.size();
         int n=mat[0].size();
-        while(!q.empty())
+        while(!q.empty())//do this till q is not empty
         {
              int i=q.front().first;
              int j=q.front().second;
-             q.pop();
-             if(checkValid(i+1,j,m,n)&& ans[i+1][j]==-1)
+             q.pop();//pop it from the queue
+             if(checkValid(i+1,j,m,n)&& ans[i+1][j]==-1)//check the adjacent cells if they are unvisited and if their adjacent neighbour is exisiting
              {
-                 ans[i+1][j]=ans[i][j]+1;
+                 ans[i+1][j]=ans[i][j]+1;//value is 1 added to the current value
                  q.push({i+1,j});
              }
              if(checkValid(i,j+1,m,n)&& ans[i][j+1]==-1)
