f11510a7a7820ebf06421f6a7848d57730a560b3
abhi-1407 <76861961+abhi-1407@users.noreply.github.com>
2022-08-04 12:24:00 +0530

    Time: 13 ms (40.01%), Space: 7 MB (74.86%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.cpp
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -3,15 +3,16 @@ public:
     bool isIsomorphic(string s, string t) {
         if(s.length()!=t.length())
             return false;
-        unordered_map<char,int> mpp1,mpp2;
+        unordered_map<char,char> mpp1,mpp2;
+        
         for(int i=0;i<s.length();i++)
         {
-           mpp1[s[i]]=i+1;
-           mpp2[t[i]]=i+1;
-        }
-        for(int i=0;i<s.length();i++)
-        {
-           if(mpp1[s[i]]!=mpp2[t[i]])
+           if(!mpp1[s[i]] and !mpp2[t[i]])
+           {
+               mpp1[s[i]]=t[i];
+               mpp2[t[i]]=s[i];
+           }
+           else if(mpp1[s[i]]!=t[i])
                return false;
         }
         
