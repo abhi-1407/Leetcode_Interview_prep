2092d3e774e2eb056441793b8295eeee6e80695c
abhi-1407 <76861961+abhi-1407@users.noreply.github.com>
2022-08-06 13:09:48 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (96.00%) - LeetHub

---
+++ 458-poor-pigs/458-poor-pigs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int poorPigs(int buckets, int mD, int mT) {
+       int t=mT/mD;
+       int m=1,ans=1;
+       for(int i=1;i<=1000;i++)
+       {
+           if(ans>=buckets)
+           {
+               m=i-1;
+               break;
+           }
+           ans*=(t+1);
+       }
+        return m;
+    }
+};
