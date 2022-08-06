c24db8a6dd6bd880f3c9012d09a8ad85bc5323e0
abhi-1407 <76861961+abhi-1407@users.noreply.github.com>
2022-08-06 13:20:58 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (22.55%) - LeetHub

--- 458-poor-pigs/458-poor-pigs.cpp
+++ 458-poor-pigs/458-poor-pigs.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int poorPigs(int buckets, int mD, int mT) {
-       int t=mT/mD;
-       int m=1,ans=1;
-       for(int i=1;i<=1000;i++)
-       {
-           if(ans>=buckets)
-           {
-               m=i-1;
-               break;
-           }
-           ans*=(t+1);
-       }
-        return m;
+       // int t=mT/mD;
+       // int m=1,ans=1;
+       // for(int i=1;i<=1000;i++)
+       // {
+       //     if(ans>=buckets)
+       //     {
+       //         m=i-1;
+       //         break;
+       //     }
+       //     ans*=(t+1);
+       // }
+       //  return m;
+    return ceil(log(buckets) / log(mT / mD + 1));//solving expression using log
     }
 };
