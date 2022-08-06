38995879a6808b21d95554a97d5f961fdbf3d762
abhi-1407 <76861961+abhi-1407@users.noreply.github.com>
2022-08-06 12:26:32 +0530

    Time: 292 ms (8.16%), Space: 33.6 MB (31.46%) - LeetHub

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -1,33 +1,58 @@
 class Solution {
 public:
     
-    
-     int minSwaps(string s) {
-      stack<char> st;
-      for(int i=0;i<s.length();i++)
-      {
-          if(s[i]=='[' )
-              st.push(s[i]);
-          else  
-          {
-              if(!st.empty() && st.top()=='[')
-                  st.pop();
-              else
-                  st.push(s[i]);
-          }
-      }
-      float a=0,b=0;
-      while(!st.empty())
-      {
-          if(st.top()=='[')
-              a++;
-          else
-              b++;
-          st.pop();
-      }
-      if(a<=b)
-          return (b-a)/2+ceil(a/2);
-       else
-          return (a-b)/2+ceil(a/2);
-     }
+//     int minSwaps(string s) {
+//       int count=0,maxcount=0;
+//       for(int i=0;i<s.length();i++)
+//       {
+//           if(s[i]==']')
+//               count++;
+//           else 
+//               count--;
+//           maxcount=max(maxcount,count);
+//       }
+//       return (maxcount+1)/2;
+//     }
+//      int minSwaps(string s) {
+//       stack<char> st;
+//       for(int i=0;i<s.length();i++)
+//       {
+//           if(s[i]=='[' )
+//               st.push(s[i]);
+//           else  
+//           {
+//               if(!st.empty() && st.top()=='[')
+//                   st.pop();
+//               else
+//                   st.push(s[i]);
+//           }
+//       }
+//       float a=0,b=0;
+//       while(!st.empty())
+//       {
+//           if(st.top()=='[')
+//               a++;
+//           else
+//               b++;
+//           st.pop();
+//       }
+//       if(a<=b)
+//           return (b-a)/2+ceil(a/2);
+//        else
+//           return (a-b)/2+ceil(a/2);
+//      }
+    int minSwaps(string s) {
+        
+        stack<char>st;
+        for(int i=0;i<s.length();i++){
+            if(s[i]==']' and st.size()){
+                if(st.top()=='[')
+                    st.pop();
+            }
+            else
+                st.push(s[i]);
+        }
+        return st.size()/2;
+        
+    }
 };
