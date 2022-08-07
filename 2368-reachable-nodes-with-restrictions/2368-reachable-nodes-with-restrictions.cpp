a18f85c592f9372c5be1dc9996406686e8e1bc42
abhi-1407 <76861961+abhi-1407@users.noreply.github.com>
2022-08-07 09:54:46 +0530

    Time: 1414 ms (11.11%), Space: 186.5 MB (11.11%) - LeetHub

--- 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.cpp
+++ 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.cpp
@@ -7,39 +7,35 @@ public:
         unordered_map<int,vector<int>> mpp;
         for(int i=0;i<edges.size();i++)
         {
-            int a=edges[i][0];
-            int b=edges[i][1];
-            if((res.find(a)==res.end()) && (res.find(b)==res.end()))
-            {
-                mpp[edges[i][0]].push_back(edges[i][1]);
-                mpp[edges[i][1]].push_back(edges[i][0]);
-            }
+            mpp[edges[i][0]].push_back(edges[i][1]);
+            mpp[edges[i][1]].push_back(edges[i][0]);
+            
                         
         }
         
         
-        if(res.count(0))return 0;
-        queue<int>q;
-        vector<int>vis(n,0);
+        if(res.count(0))return 0;//check if zero is restricted
+        queue<int>q;//queue for bfs
+        vector<int>vis(n,0);//visted array to keep a track of visited one
         
-        q.push(0);
-        vis[0]=1;        
-        int count=0;
+        q.push(0);//push the starting node
+        vis[0]=1;//mark visited as 1  
+        int count=0;//initialise counter as 0
         while(!q.empty())
         {
             int ele=q.front();
-            q.pop();
-            if(res.count(ele))continue;
-            count++;
-            for(auto it:mpp[ele])
+            q.pop();//pop the first element from the queue
+            if(res.count(ele))continue;//if this element is restricted then continue
+            count++;//else increment the counter
+            for(auto it:mpp[ele])//check for the neighbours
             {
-                if(vis[it]==0)
+                if(vis[it]==0)//if the neighbour is not visited
                 {
-                   vis[it]=1;
-                   q.push(it);
+                   vis[it]=1;//mark it as 1
+                   q.push(it);//push it into the queue
                 }
             }
         }
-        return count;
+        return count;//return the count
     }
 };
