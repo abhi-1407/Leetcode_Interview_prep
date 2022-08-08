694744de27dd68f7613a09a1686dbac4ad844033
abhi-1407 <76861961+abhi-1407@users.noreply.github.com>
2022-08-08 20:35:53 +0530

    Time: 20 ms (88.64%), Space: 11 MB (50.93%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,28 +1,42 @@
 class Solution {
 
 public:
-    string removeDuplicates(string s) {
-    stack<char> st;
-    if(s.length()==1 || s.length()==0)
-        return s;
-    st.push(s[0]);    
-    for(int i=1;i<s.length();i++)
-    {
-      if(st.empty())
-          st.push(s[i]);
-      else if((st.top()==s[i])&&(!st.empty()))
-          st.pop();
-      else
-      st.push(s[i]);
+//     string removeDuplicates(string s) {
+//     stack<char> st;
+//     if(s.length()==1 || s.length()==0)
+//         return s;
+//     st.push(s[0]);    
+//     for(int i=1;i<s.length();i++)
+//     {
+//       if(st.empty())
+//           st.push(s[i]);
+//       else if((st.top()==s[i])&&(!st.empty()))
+//           st.pop();
+//       else
+//       st.push(s[i]);
       
-    }
-    string ans="";
-    while(!st.empty())
+//     }
+//     string ans="";
+//     while(!st.empty())
+//     {
+//         ans+=st.top();
+//         st.pop();
+//     }
+//     reverse(ans.begin(),ans.end());
+//     return ans;
+//     }
+    
+    //space optimised --->O(n) and space complexity ----> O(1)
+    string removeDuplicates(string s)
     {
-        ans+=st.top();
-        st.pop();
-    }
-    reverse(ans.begin(),ans.end());
-    return ans;
+        string ans="";
+        for(int i=0;i<s.length();i++)
+        {
+            if(ans.back()==s[i])
+                ans.pop_back();
+            else
+                ans.push_back(s[i]);
+        }
+        return ans;
     }
 };
