5fd99b5d0812e9568763ba674b3cc6b60e40273f
abhi-1407 <76861961+abhi-1407@users.noreply.github.com>
2022-08-12 12:37:31 +0530

    Time: 48 ms (47.20%), Space: 23.3 MB (12.34%) - LeetHub

---
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+    
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root)
+            return NULL;
+        /* current node's value is smaller so we'll move right */
+        if(root->val<p->val && root->val<q->val)
+            return lowestCommonAncestor(root->right,p,q);
+        /* current node's value is greater so we'll move left */
+        else if(root->val>p->val && root->val>q->val)
+            return lowestCommonAncestor(root->left,p,q);//
+        else if((root->val > p->val && root->val < q->val )||(root->val < p->val && root->val > q->val )|| (root->val==q->val)||(root->val==p->val))
+            return root;//point of split/intersection
+        return NULL;
+    }
+};
