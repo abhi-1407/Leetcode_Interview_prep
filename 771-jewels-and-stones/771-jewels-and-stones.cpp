474a260587109e787816522f68509b43d561c227
abhi-1407 <76861961+abhi-1407@users.noreply.github.com>
2022-08-12 19:49:06 +0530

    Time: 4 ms (44.34%), Space: 6.3 MB (45.22%) - LeetHub

---
+++ 771-jewels-and-stones/771-jewels-and-stones.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        unordered_map<char,int> mpp;
+        int count=0;
+        for(int i=0;i<stones.size();i++)
+        {
+            mpp[stones[i]]++;
+        }
+        for(int i=0;i<jewels.size();i++)
+        {
+            if(mpp.find(jewels[i])!=mpp.end())
+            {
+                count+=mpp[jewels[i]];
+            }
+        }
+        return count;
+        
+    }
+};
