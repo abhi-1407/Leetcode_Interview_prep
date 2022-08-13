f0b8f7469a89b9aafbafe298154edaeebb13ed9e
abhi-1407 <76861961+abhi-1407@users.noreply.github.com>
2022-08-13 23:11:36 +0530

    Time: 59 ms (25.95%), Space: 25.5 MB (40.72%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -1,19 +1,15 @@
 class Solution {
-    void reverse(vector<int> &arr,int i,int j)//reversing function
-    {
-        while(i<j)
-        {
-            swap(arr[i++],arr[j--]);
-        }
-    }
+    
 public:
     void rotate(vector<int>& nums, int k) {
         int n=nums.size();
 /* if the no. of rotations are a lot then ultimately remainder of that value with n would be give the same ans*/
         k=k%n;
-        reverse(nums,0,n-k-1); // reverse the part from 0 to k(from behind) 
-        reverse(nums,n-k,n-1);//reverse the part from k(from behind) to n
-        reverse(nums,0,n-1);//reverse the entire array
-        
+        vector<int> temp(n);
+        for(int i=0;i<n;i++)
+        {
+            temp[(i+k)%n]=nums[i];
+        }
+        nums=temp;
     }
 };
