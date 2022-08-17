e2bbb0a0e557f0519d9a6de7b6ed50ab89a9f1a0
abhi-1407 <76861961+abhi-1407@users.noreply.github.com>
2022-08-18 00:17:01 +0530

    Time: 46 ms (95.32%), Space: 16 MB (67.83%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -1,5 +1,5 @@
 class Solution {
-    bool isValid(vector<vector<int>>& matrix,int i,int j)
+    bool isValid(vector<vector<int>>& matrix,int i,int j)//to check if that position is valid
     {
         if(i<0 || j<0 || i>matrix.size()-1 || j>matrix[0].size()-1)
             return false;
@@ -7,7 +7,7 @@ class Solution {
     }
     int helper(int i,int j,vector<vector<int>>& matrix,vector<vector<int>> &dp)
     {
-        if(dp[i][j]!=-1)
+        if(dp[i][j]!=-1)//if that state is previously visited then use the already stored value
             return dp[i][j];
         int left=0,right=0,up=0,down=0;
         if(isValid(matrix,i+1,j) && matrix[i+1][j]> matrix[i][j] )
@@ -27,7 +27,7 @@ class Solution {
         {
              right=helper(i,j+1,matrix,dp);              
         }
-        return dp[i][j]=1+max(max(left,right),max(up,down));  
+        return dp[i][j]=1+max(max(left,right),max(up,down));  //return 1 to max to all the calls
     }
 public:
     int longestIncreasingPath(vector<vector<int>>& matrix) {
@@ -39,7 +39,7 @@ public:
         {
             for(int j=0;j<matrix[i].size();j++)
             {
-                maxi=max(maxi,helper(i,j,matrix,dp));
+                maxi=max(maxi,helper(i,j,matrix,dp));//calculating max increasing path by taking different starting points
             }
         }
         return maxi;
