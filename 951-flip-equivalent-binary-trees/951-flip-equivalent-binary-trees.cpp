1de559dbf8967fea585bc763bad2134d260a5c5f
abhi-1407 <76861961+abhi-1407@users.noreply.github.com>
2022-08-17 18:07:14 +0530

    Time: 0 ms (100.00%), Space: 12.4 MB (5.87%) - LeetHub

--- 951-flip-equivalent-binary-trees/951-flip-equivalent-binary-trees.cpp
+++ 951-flip-equivalent-binary-trees/951-flip-equivalent-binary-trees.cpp
@@ -16,10 +16,10 @@ public:
             return true;
         if((!root1 and root2) || (root1 and !root2))
             return false;
-        if(root1->val!=root2->val)
+        if(root1->val!=root2->val)//if values are different then they can never be same in this configuration
             return false;
-        bool a=flipEquiv(root1->left,root2->left) and flipEquiv(root1->right,root2->right);
-        bool b=flipEquiv(root1->left,root2->right) and flipEquiv(root1->right,root2->left);
+        bool a=flipEquiv(root1->left,root2->left) and flipEquiv(root1->right,root2->right);//if they are identical
+        bool b=flipEquiv(root1->left,root2->right) and flipEquiv(root1->right,root2->left);//if they are mirror of each other
         return a or b;
     }
 };
