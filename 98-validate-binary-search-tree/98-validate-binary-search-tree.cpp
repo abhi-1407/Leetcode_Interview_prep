c9d8b9c8c1fc0d4d1d724da691ed243149163c88
abhi-1407 <76861961+abhi-1407@users.noreply.github.com>
2022-08-24 13:14:37 +0530

    Time: 15 ms (77.39%), Space: 21.7 MB (30.99%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -9,29 +9,27 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
-
 class Solution {
-private:
-    bool isBst(TreeNode *root,long long min,long long max)
-   {
-    if(root==NULL)
-        return true;
-    if(root->val <= min || root->val >= max)//if the root's value is not in the range
+bool helper(TreeNode *root,long long min,long long max)
+{
+    if(!root)
+       return true;
+    if(root->val > min and root->val < max)
     {
-        return false;
-    }
-    /* if we are reaching till this point then it is in the range so check for left and 
-    right trees */
-    return isBst(root->left,min,root->val) && isBst(root->right,root->val,max) ;
-   }
+        int l=helper(root->left,min,root->val);
+        int r=helper(root->right,root->val,max);
+        return l&&r;
+    }        
+    return false;        
     
+    
+ }
 public:
     bool isValidBST(TreeNode* root) {
+        long long  l=-3000000000;
+        long long r=3000000000;
+        return helper(root,l,r);
         
-        long long minV = -30000000000;//as values can be very large
-        long long maxV = 30000000000;//as values can be very large
-        return isBst(root,minV,maxV);
         
     }
 };
