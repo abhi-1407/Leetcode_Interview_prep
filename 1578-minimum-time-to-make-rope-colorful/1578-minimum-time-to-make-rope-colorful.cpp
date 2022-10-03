23760b1dc1c5af73449ec727b3e1770e2acfea5f
abhi-1407 <76861961+abhi-1407@users.noreply.github.com>
2022-10-04 00:07:24 +0530

    Time: 365 ms (24.42%), Space: 95.3 MB (84.72%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -1,55 +1,71 @@
 class Solution {
 public:
     int minCost(string colors, vector<int>& neededTime) {
-        int n=colors.length();
-        if(n==0 || n==1)
-          return 0;
-        char curr;
-        int cost=0;
-        int i=0,j=0;
-        priority_queue<int,vector<int>,greater<int>> pq;
-        while(i<n && j<n)
-        {
-            if(colors[i]==colors[j])
-            {
-                pq.push(neededTime[j]);
-                j++;
-            }
-            else
-            {
+//         int n=colors.length();
+//         if(n==0 || n==1)
+//           return 0;
+//         char curr;
+//         int cost=0;
+//         int i=0,j=0;
+//         priority_queue<int,vector<int>,greater<int>> pq;
+//         while(i<n && j<n)
+//         {
+//             if(colors[i]==colors[j])
+//             {
+//                 pq.push(neededTime[j]);
+//                 j++;
+//             }
+//             else
+//             {
                 
-                if(pq.size()>1)
-                {
+//                 if(pq.size()>1)
+//                 {
                     
-                    while(pq.size()!=1)
-                    {
-                        cout<<pq.top()<<" ";
-                        cost+=pq.top();
-                        pq.pop();
-                    }
-                    pq.pop();
-                    i=j;
-                }
-                else if(pq.size()==1)
-                {
-                    pq.pop();
-                    i=j;
-                }
+//                     while(pq.size()!=1)
+//                     {
+//                         cout<<pq.top()<<" ";
+//                         cost+=pq.top();
+//                         pq.pop();
+//                     }
+//                     pq.pop();
+//                     i=j;
+//                 }
+//                 else if(pq.size()==1)
+//                 {
+//                     pq.pop();
+//                     i=j;
+//                 }
                                 
+//             }
+//         }
+//             if(pq.size()>1)
+//             {
+//                while(pq.size()!=1)
+//                {
+//                   cout<<pq.top()<<" ";
+//                   cost+=pq.top();
+//                   pq.pop();
+//                }
+//                pq.pop();
+//             }            
+        
+//         return cost;
+        int cost=0;
+        for(int i=1;i<neededTime.size();i++)
+        {
+            if(colors[i]==colors[i-1])
+            {
+                if(neededTime[i]<neededTime[i-1])
+                {
+                    cost+=neededTime[i];
+                    neededTime[i]=neededTime[i-1];
+                }
+                else
+                {
+                    cost+=neededTime[i-1];
+                }
             }
         }
-            if(pq.size()>1)
-            {
-               while(pq.size()!=1)
-               {
-                  cout<<pq.top()<<" ";
-                  cost+=pq.top();
-                  pq.pop();
-               }
-               pq.pop();
-            }            
-        
         return cost;
-        
     }
 };
