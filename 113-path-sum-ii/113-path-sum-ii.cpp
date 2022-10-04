691a11104131bfc29195fcce0744458671734c23
abhi-1407 <76861961+abhi-1407@users.noreply.github.com>
2022-10-04 09:48:43 +0530

    Time: 25 ms (37.21%), Space: 38.9 MB (7.77%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.cpp
+++ 113-path-sum-ii/113-path-sum-ii.cpp
@@ -12,27 +12,28 @@
 class Solution {
     void helper(TreeNode *root,vector<int> temp,vector<vector<int>> &ans,int targetSum)
     {
-        if(!root)
+        if(!root)//trivial case
             return;
-        if(root->left==NULL and root->right==NULL)
+        if(root->left==NULL and root->right==NULL)//leaf node
         {
-            temp.push_back(root->val);
+            temp.push_back(root->val);//push the current node
             int sum=0;
-            for(auto it:temp)
+            for(auto it:temp)//check the sum
             {
                 sum+=it;
             }
-            if(sum==targetSum)
+            if(sum==targetSum)//if its equal to the targetSum then push this vector and return
             {
                 ans.push_back(temp);
                 return;
             }
             
         }
-        temp.push_back(root->val);
-        helper(root->left,temp,ans,targetSum);
+        /* no need to pop as we are passing by value */
+        temp.push_back(root->val);//push the current node's value into the vector
+        helper(root->left,temp,ans,targetSum);//call on the left node
         helper(root->right,temp,ans,targetSum);
-        temp.pop_back();
+        
     }
 public:
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
