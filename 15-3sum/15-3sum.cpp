9f9ebeb2ab96875450eb75b3c74b7d3808b3aab9
abhi-1407 <76861961+abhi-1407@users.noreply.github.com>
2022-10-06 09:57:17 +0530

    Time: 355 ms (21.66%), Space: 23.5 MB (26.14%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -33,41 +33,84 @@ class Solution {
 //          ans.push_back(temp);
 //      }
 //      return ans;
+    
+   
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-     sort(nums.begin(),nums.end());
-     vector<vector<int>> ans;
-     for(int i=0;i<nums.size()-2;i++)
-     {
-         if(i>0 and nums[i-1]==nums[i])continue;
-         int l=i+1;
-         int h=nums.size()-1;
-         while(l<h)
-         {
-             if(nums[l]+nums[h]+nums[i]==0)
-             {
-                 vector<int> temp;
-                 temp.push_back(nums[i]);
-                 temp.push_back(nums[l]);
-                 temp.push_back(nums[h]);
-                 ans.push_back(temp);
-                 while(l<h && nums[l]==nums[l+1])l++;
-                 while(l<h && nums[h]==nums[h-1])h--;
-                 l++;
-                 h--;
-                 //break;
-             }
-             else if(nums[l]+nums[h]+nums[i] < 0)
-             {
-                 l++;
-             }
-             else
-             {
-                 h--;
-             }
-         }
-     }
-     return ans;
+    // vector<vector<int>> threeSum(vector<int>& nums) {
+    //  sort(nums.begin(),nums.end());
+    //  vector<vector<int>> ans;
+    //  for(int i=0;i<nums.size()-2;i++)
+    //  {
+    //      if(i>0 and nums[i-1]==nums[i])continue;
+    //      int l=i+1;
+    //      int h=nums.size()-1;
+    //      while(l<h)
+    //      {
+    //          if(nums[l]+nums[h]+nums[i]==0)
+    //          {
+    //              vector<int> temp;
+    //              temp.push_back(nums[i]);
+    //              temp.push_back(nums[l]);
+    //              temp.push_back(nums[h]);
+    //              ans.push_back(temp);
+    //              while(l<h && nums[l]==nums[l+1])l++;
+    //              while(l<h && nums[h]==nums[h-1])h--;
+    //              l++;
+    //              h--;
+    //              //break;
+    //          }
+    //          else if(nums[l]+nums[h]+nums[i] < 0)
+    //          {
+    //              l++;
+    //          }
+    //          else
+    //          {
+    //              h--;
+    //          }
+    //      }
+    //  }
+    //  return ans;
+    
+     vector<vector<int>> threeSum(vector<int>& nums){
+       sort(nums.begin(),nums.end());
+       vector<vector<int>> ans;
+       for(int i=0;i<nums.size()-2;i++)
+       {
+           if(i>0 and nums[i]==nums[i-1])
+           continue;               
+           int l=i+1;
+           int h=nums.size()-1;           
+           while(l<h)
+           {
+               if(nums[l]+nums[h]+nums[i]==0)
+               {
+                   vector<int> temp;
+                   temp.push_back(nums[l]);
+                   temp.push_back(nums[i]);
+                   temp.push_back(nums[h]); 
+                   ans.push_back(temp);
+                   while(l<h && nums[l]==nums[l+1])l++;
+                   while(l<h && nums[h]==nums[h-1])h--;
+                   l++;
+                   h--;
+               }
+               else if(nums[l]+nums[h]+nums[i] > 0)
+               {
+                   while(l<h && nums[h]==nums[h-1])h--;
+                   h--;
+               }
+               else
+               {
+                   while(l<h && nums[l]==nums[l+1])l++;
+                   l++;
+               }
+           }
+           
+       }
+         return ans;
+        
+    }
+    
        
-}
+
 };
