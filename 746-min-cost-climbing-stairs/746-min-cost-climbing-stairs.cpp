5fa0be00121ccde17b11873fb27a8d16507ef98c
abhi-1407 <76861961+abhi-1407@users.noreply.github.com>
2022-10-07 10:14:58 +0530

    Time: 10 ms (66.51%), Space: 15.2 MB (6.40%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -5,9 +5,11 @@ class Solution {
             return cost[index];
         if(dp[index]!=-1)
             return dp[index];
-        int one=helper(cost,index-1,dp);
-        int two=helper(cost,index-2,dp);
-        return dp[index]=min(one,two)+cost[index];
+        int one=cost[index]+helper(cost,index-1,dp);
+        int two=INT_MAX;
+        if(index>=2)
+        two=cost[index]+helper(cost,index-2,dp);
+        return dp[index]=min(one,two);
     }
 public:
     int minCostClimbingStairs(vector<int>& cost) {
