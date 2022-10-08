eb4fd641c230b0f269e4970c351ddbc438995673
abhi-1407 <76861961+abhi-1407@users.noreply.github.com>
2022-10-08 10:30:38 +0530

    Time: 606 ms (26.42%), Space: 16.4 MB (48.38%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.cpp
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -1,36 +1,62 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-        sort(nums.begin(),nums.end());
-        int n=nums.size();
-        int diff=INT_MAX;
-        int ans=0;
-        for(int i=0;i<n;i++)
-        {
-            int ele=nums[i];
-            int j=i+1,k=n-1;
-            while(j<k)
-            {
-                if(ele+nums[j]+nums[k]==target)
-                {
-                    return target;
-                }
-                else if(abs((ele+nums[j]+nums[k])-target)<diff)
-                {
-                    diff=abs((ele+nums[j]+nums[k])-target);
-                    ans=ele+nums[j]+nums[k];
-                }
-                if((ele+nums[j]+nums[k])>target)
-                {
-                    k--;
-                }
-                else
-                {
-                    j++;
-                }
+//         sort(nums.begin(),nums.end());
+//         int n=nums.size();
+//         int diff=INT_MAX;
+//         int ans=0;
+//         for(int i=0;i<n;i++)
+//         {
+//             int ele=nums[i];
+//             int j=i+1,k=n-1;
+//             while(j<k)
+//             {
+//                 if(ele+nums[j]+nums[k]==target)
+//                 {
+//                     return target;
+//                 }
+//                 else if(abs((ele+nums[j]+nums[k])-target)<diff)
+//                 {
+//                     diff=abs((ele+nums[j]+nums[k])-target);
+//                     ans=ele+nums[j]+nums[k];
+//                 }
+//                 if((ele+nums[j]+nums[k])>target)
+//                 {
+//                     k--;
+//                 }
+//                 else
+//                 {
+//                     j++;
+//                 }
                     
-            }
+//             }
+//         }
+//         return ans;
+//     }
+        sort(nums.begin(),nums.end());
+int ans=INT_MAX,mini=INT_MAX;
+for(int i=0;i<nums.size();i++)
+{
+    int j=i+1,k=nums.size()-1;
+    while(j<k)
+    {
+        if(nums[i]+nums[j]+nums[k]==target)
+        {
+            return target;
         }
-        return ans;
+        if(abs(target-(nums[i]+nums[j]+nums[k]))<mini)
+        {
+            ans=nums[i]+nums[j]+nums[k];
+            mini=abs(target-(nums[i]+nums[j]+nums[k]));
+        }
+        if(nums[i]+nums[j]+nums[k]>target)
+            k--;
+        else
+            j++;        
     }
+    
+}
+return ans;
+}
 };
+
