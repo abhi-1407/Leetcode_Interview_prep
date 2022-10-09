6ffea4b0c8a9f19a327bbefa78d370677c67d479
abhi-1407 <76861961+abhi-1407@users.noreply.github.com>
2022-10-09 22:03:03 +0530

    Time: 108 ms (17.23%), Space: 18.4 MB (53.00%) - LeetHub

--- 518-coin-change-ii/518-coin-change-ii.cpp
+++ 518-coin-change-ii/518-coin-change-ii.cpp
@@ -1,23 +1,50 @@
 class Solution {
-    int counter(int amount,vector<int> coins,int index,vector<vector<int>> &dp)
+//     int counter(int amount,vector<int> coins,int index,vector<vector<int>> &dp)
+//     {
+//         if(index==0)
+//         {
+//             return (amount%coins[index]==0);
+//         }
+//         if(dp[index][amount]!=-1)
+//             return dp[index][amount];
+//         int pick=0;
+//         if(coins[index]<=amount)
+//         {
+//             pick=counter(amount-coins[index],coins,index,dp);
+//         }
+//         int notpick=counter(amount,coins,index-1,dp);
+//         return dp[index][amount]=pick+notpick;
+//     }
+    
+    int HelperTab(int a,vector<int> &coins)
     {
-        if(index==0)
+        int n=coins.size();
+        vector<vector<int>> dp(n,vector<int>(a+1,0));
+        for(int index=0;index<n;index++)
         {
-            return (amount%coins[index]==0);
+            for(int amount=0;amount<=a;amount++)
+            {
+                if(index==0)
+                {
+                    dp[index][amount]=(amount%coins[index]==0);
+                }
+                else
+                {
+                    int pick=0;
+                    if(coins[index]<=amount)
+                        pick=dp[index][amount-coins[index]];
+                    int notpick=dp[index-1][amount];
+                    dp[index][amount]=pick+notpick;
+                }
+            }
         }
-        if(dp[index][amount]!=-1)
-            return dp[index][amount];
-        int pick=0;
-        if(coins[index]<=amount)
-        {
-            pick=counter(amount-coins[index],coins,index,dp);
-        }
-        int notpick=counter(amount,coins,index-1,dp);
-        return dp[index][amount]=pick+notpick;
+        return dp[n-1][a];       
+        
     }
 public:
     int change(int amount, vector<int>& coins) {
-        vector<vector<int>> dp(coins.size(),vector<int>(amount+1,-1));
-        return counter(amount,coins,coins.size()-1,dp);        
+        return HelperTab(amount,coins);
+        // vector<vector<int>> dp(coins.size(),vector<int>(amount+1,-1));
+        // return counter(amount,coins,coins.size()-1,dp);        
     }
 };
