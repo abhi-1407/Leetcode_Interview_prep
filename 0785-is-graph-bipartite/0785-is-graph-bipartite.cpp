c76020ad7ee26f3f693a10426eb9d3106836114d
abhi-1407 <76861961+abhi-1407@users.noreply.github.com>
2022-10-18 19:50:37 +0530

    Time: 81 ms (8.29%), Space: 18.2 MB (5.83%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,52 +1,42 @@
 class Solution {
-    bool helper(vector<int> &color,vector<vector<int>> &graph,int i,vector<int> adj[])
+    bool dfs(vector<int> &color,int node,vector<int> adj[],int c)
     {
-        queue<int> q;
-        q.push(i);//push the starting node into the queue
-        color[i]=0;//start with one color say 0
-        while(!q.empty())
+        color[node]=c;
+        for(auto it:adj[node])
         {
-            int node=q.front();
-            q.pop();
-            int nodecolor=color[node];
-            for(auto it:adj[node])
+            if(color[it]==-1)
             {
-                if(color[it]==-1)
-                {
-                    color[it]=!nodecolor;//color it with the opp color
-                    q.push(it);//push it in to the queue
-                }
-                else if(color[it]==nodecolor)//if adj nodes get the same color
-                {
+                if(dfs(color,it,adj,!c)==false)
                     return false;
-                } 
+            }
+            else if(color[it]==color[node])
+            {
+                return false;
             }
         }
         return true;
     }
 public:
     bool isBipartite(vector<vector<int>>& graph) {
-        queue<int> p;
         int n=graph.size();
         vector<int> color(n,-1);
         vector<int> adj[n];
         for(int i=0;i<graph.size();i++)
         {
-            for(int j=0;j<graph[i].size();j++)//as it not a n*n matrix
+            for(int j=0;j<graph[i].size();j++)
             {
                 adj[i].push_back(graph[i][j]);
                 adj[graph[i][j]].push_back(i);
             }
         }
-        for(int i=0;i<graph.size();i++)
+        for(int i=0;i<n;i++)
         {
             if(color[i]==-1)
             {
-                if(helper(color,graph,i,adj)==false)
-                    return false;
+                if(dfs(color,i,adj,0)==false)
+                    return false;                    
             }
         }
-        return true;       
-        
+        return true;
     }
 };
