8d8734031d342b23c1a29a5299fd0bdd96c0ab18
abhi-1407 <76861961+abhi-1407@users.noreply.github.com>
2022-10-21 21:12:06 +0530

    Time: 731 ms (50.70%), Space: 10.4 MB (53.41%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -13,12 +13,34 @@ class Solution {
         }
         return dp[i][j]=maxi;
     }
+    int DP(vector<int> nums,int n)
+    {
+        vector<vector<int>> dp(n+2,vector<int>(n+2,0));
+        for(int i=n;i>=1;i--)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(i>j)continue;
+                int maxi=INT_MIN;
+                for(int index=i;index<=j;index++)
+                {
+                     int cost=(nums[index]*nums[i-1]*nums[j+1])+dp[i][index-1]+dp[index+1][j];
+                     maxi=max(maxi,cost);
+                }
+                dp[i][j]=maxi;
+                
+            }
+        }
+        return dp[1][n];       
+
+    }
 public:
     int maxCoins(vector<int>& nums) {
         int n=nums.size();
         nums.push_back(1);
         nums.insert(nums.begin(),1);
-        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-        return helper(1,n,nums,dp);
+        // vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        // return helper(1,n,nums,dp);
+        return DP(nums,n);
     }
 };
