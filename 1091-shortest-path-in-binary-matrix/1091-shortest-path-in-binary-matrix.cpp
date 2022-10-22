f6a510de6cd155e5a50600ab882515f731ee7da7
abhi-1407 <76861961+abhi-1407@users.noreply.github.com>
2022-10-22 13:25:14 +0530

    Time: 65 ms (94.97%), Space: 20.9 MB (52.02%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,45 +1,38 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        if(grid[0][0]==0 and grid.size()==1 and grid[0].size()==1)
-            return 1;
-        if(grid[0][0]==1)
-            return -1;
         int n=grid.size();
         int m=grid[0].size();
         vector<vector<int>> distance(n,vector<int>(m,1e9));
-        distance[0][0]=0;
-        queue<pair<int,pair<int,int>>> q;
-        q.push({1,{0,0}});
-        int dr[]={-1,0,1,0,-1,-1,1,1};
-        int dc[]={0,1,0,-1,-1,1,-1,1};
-        while(!q.empty())
+        queue<pair<int,int>> q;
+        int dr[]={1,0,-1,0,-1,1,-1,1};
+        int dc[]={0,1,0,-1,1,-1,-1,1};
+        if(grid[0][0]==0)
         {
-            auto temp=q.front();
-            q.pop();
-            int dist=temp.first;
-            int x=temp.second.first;
-            int y=temp.second.second;
-            for(int i=0;i<8;i++)
-            {
-                int newx=dr[i]+x;
-                int newy=dc[i]+y;
-                if(newx<0 || newy<0 || newx>=n || newy>=m)
-                    continue;
-                else
-                {
-                    if((distance[newx][newy] > dist+1) and grid[newx][newy]==0)
-                    {
-                        if(newx==n-1 && newy==m-1)
-                            return 1+dist;
-                        distance[newx][newy]=dist+1;
-                        q.push({dist+1,{newx,newy}});
-                    }
-                }
-            }            
+            distance[0][0]=1;
+            q.push({0,0});
         }
         
-        return -1;    
-        
+        while(!q.empty())
+        {
+           auto temp=q.front();
+           q.pop();
+           int x=temp.first;
+           int y=temp.second;
+           for(int i=0;i<8;i++)
+           {
+               int newx=x+dr[i];
+               int newy=y+dc[i];
+               if(newx<0 || newy<0 || newx>=n || newy>=m || grid[newx][newy]==1)
+                   continue;
+               if(distance[x][y]+1 < distance[newx][newy])
+               {
+                   distance[newx][newy]=1+distance[x][y];
+                   q.push({newx,newy});
+               }
+               
+           }            
+        }
+        return (distance[n-1][m-1]==1e9)?-1:distance[n-1][m-1];
     }
 };
