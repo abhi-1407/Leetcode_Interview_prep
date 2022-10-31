bc22e1e2a465a3610ba1005e2c49c8280aac2f2d
abhi-1407 <76861961+abhi-1407@users.noreply.github.com>
2022-10-31 17:56:40 +0530

    Time: 63 ms (61.20%), Space: 15.2 MB (56.25%) - LeetHub

--- 0218-the-skyline-problem/0218-the-skyline-problem.cpp
+++ 0218-the-skyline-problem/0218-the-skyline-problem.cpp
@@ -1,41 +1,44 @@
 class Solution {
 public:
     vector<vector<int>> getSkyline(vector<vector<int>>& buildings) {
-         multiset<int,greater<int>> ms;
-         vector<pair<int,int>> heights;
-         for(auto it:buildings)
-         {
-             heights.push_back({it[0],-it[2]});//+ve represents the left edge of the building
-             heights.push_back({it[1],+it[2]});//-ve represents the right edge of the building
-         }
-        sort(heights.begin(),heights.end());//so that we have points in a line(they are in order)
-        ms.insert(0);
-        vector<vector<int>> ans;
-        int OngoingHeight=0;
-        for(auto it:heights)
+        vector<pair<int,int>>heights;
+        for(auto it : buildings)
         {
-            int currPoint=it.first;
-            int currHeight=it.second;
-            if(currHeight<0)
-            {
-                ms.insert(-currHeight);
-            
-            }
-            else
-            {
-                ms.erase(ms.find(currHeight));
-            }
-            //if the max height has changed then this should be in our ans            
-            int NewMax =*ms.begin();
-            if(NewMax!=OngoingHeight)
-            {
-                OngoingHeight=NewMax;
-                ans.push_back({currPoint,OngoingHeight});                
-                   
-            }
-                    
+            heights.push_back({it[0],-it[2]});
+            heights.push_back({it[1], it[2]});
         }
-        return ans;
         
+		// sorting the heights according to the occurences
+        sort(heights.begin(), heights.end());
+        
+        vector<vector<int>> ans;
+        
+		// multiset to store the heights in decreasing order(priority queue)
+        multiset<int, greater<int>> ms;
+        ms.insert(0);
+		
+        int preMax = 0;
+        
+        for(auto it : heights)
+        {
+			// if height is negative then adding to the set
+            if(it.second < 0)
+                ms.insert(-it.second);
+				
+			// if height is positive then removing from the set
+            else
+                ms.erase(ms.find(it.second));
+            
+            int curMax = *(ms.begin());
+            
+			// if current max changes it will add the coordinates to the ans
+            if(curMax != preMax)
+            {
+                ans.push_back({it.first, curMax});
+                preMax = curMax;
+            }
+        }
+        
+        return ans;
     }
 };
