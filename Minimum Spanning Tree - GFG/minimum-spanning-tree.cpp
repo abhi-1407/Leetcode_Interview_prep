830319b472c3ea5dbe69fba1f920000b20747a7d
abhi-1407 <76861961+abhi-1407@users.noreply.github.com>
2022-11-01 10:34:38 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -3,35 +3,79 @@
 using namespace std;
 
 // } Driver Code Ends
+
+
+class Disjoint{
+public:
+vector<int> parent,rank;
+Disjoint(int nodes)
+{
+    rank.resize(nodes+1,0);
+    parent.resize(nodes+1);
+    for(int i=0;i<nodes+1;i++)
+    parent[i]=i;
+}
+
+int findParent(int node)
+{
+    if(node==parent[node])
+        return node;
+    return parent[node]=findParent(parent[node]);    
+}
+
+void UnionBySize(int u,int v)
+{
+    int ul_u=findParent(u);
+    int ul_v=findParent(v);
+    if(ul_u==ul_v)return;
+    if(rank[ul_u]<rank[ul_v])
+    {
+        parent[ul_u]=ul_v;
+        rank[ul_v]+=rank[ul_u];
+
+    }
+    else if(rank[ul_u]>rank[ul_v])
+    {
+        parent[ul_v]=ul_u;
+        rank[ul_u]+=rank[ul_v];
+    }
+    else
+    {
+        parent[ul_v]=ul_u;
+        rank[ul_u]+=rank[ul_v];
+    }
+}
+};
+
 class Solution
 {
 	public:
-	//Function to find sum of weights of edges of the Minimum Spanning Tree.
+	//function to find sum of weights of edges of the Minimum Spanning Tree.
     int spanningTree(int V, vector<vector<int>> adj[])
     {
         // code here
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        vector<int> vis(V,-1);
-        pq.push({0,0});
-        int sum=0;
-        while(!pq.empty())
+        vector<pair<int,pair<int,int>>> nodes;
+        Disjoint ds(V-1);
+        for(int i=0;i<V;i++)
         {
-            auto it=pq.top();
-            pq.pop();
-            int weight=it.first;
-            int node=it.second;
-            if(vis[node]==1)
-            continue;
-            vis[node]=1;
-            sum+=weight;
-            for(auto it:adj[node])
+            for(int j=0;j<adj[i].size();j++)
             {
-                int n=it[0];
-                int w=it[1];
-                if(vis[n]==-1)
-                {
-                    pq.push({w,n});
-                }
+                int x=i;
+                int y=adj[i][j][0];
+                int w=adj[i][j][1];
+                nodes.push_back({w,{x,y}});
+            }
+            
+        }
+        sort(nodes.begin(),nodes.end());
+        int sum=0;
+        for(auto it:nodes)
+        {
+            int x=it.second.first,y=it.second.second,w=it.first;
+            if(ds.findParent(x)!=ds.findParent(y))
+            {
+                ds.UnionBySize(x,y);
+                sum+=w;
             }
         }
         return sum;
