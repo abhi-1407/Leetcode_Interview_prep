21e7781f93f025374fd2d821d3bcf3e1e521a907
abhi-1407 <76861961+abhi-1407@users.noreply.github.com>
2022-11-18 18:32:58 +0530

    Time: 20 ms (32.81%), Space: 6 MB (31.48%) - LeetHub

--- 0223-rectangle-area/0223-rectangle-area.cpp
+++ 0223-rectangle-area/0223-rectangle-area.cpp
@@ -1,21 +1,17 @@
+/* Intuition:
+If there's no intersection b/w the points then the x length/y length would come out to be negative,and hence we'll take common area as 0 in that case
+min and max's difference would always come out to be 0 or negative
+*/
 class Solution {
 public:
     int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
-
-        int AreaOfA = (ax2-ax1)*(ay2-ay1);
-        int AreaOfB= (bx2-bx1)*(by2-by1);
-
-        int overlapR =0;
-
-        // x overlap.
-        int xoverlap = min(ax2,bx2) - max(ax1,bx1);
-        int yoverlap = min(ay2,by2) - max(ay1,by1);
-
-        if(xoverlap>0 && yoverlap>0)
-        overlapR = xoverlap*yoverlap;
-
-        return AreaOfA+AreaOfB-overlapR;
-
-
+        int A1=(ay2-ay1)*(ax2-ax1);
+        int A2=(by2-by1)*(bx2-bx1);
+        int common=0;
+        int yover=(min(by2,ay2)-max(ay1,by1));
+        int xover=(min(ax2,bx2)-max(bx1,ax1));
+        if(xover>0 and yover>0)
+            common=xover*yover;
+        return A1+A2-common;
     }
 };
