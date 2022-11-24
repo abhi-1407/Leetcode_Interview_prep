ec9ee88930090d260dd4f6c3c53c3b2d2714a9e6
abhi-1407 <76861961+abhi-1407@users.noreply.github.com>
2022-11-24 18:03:51 +0530

    Time: 399 ms (78.20%), Space: 7.8 MB (94.48%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,28 +1,27 @@
 class Solution {
-    bool helper(int i,int j,vector<vector<char>> &board, string word,int index)
+    bool dfs(vector<vector<char>> &board,int i,int j,int index,string word)
     {
         if(index==word.size()-1)
         {
             return true;
         }
-        int dr[]={1,0,-1,0};
-        int dc[]={0,1,0,-1};
-        int ch=board[i][j];
+        char ch=board[i][j];
         board[i][j]='#';
+        int dr[]={-1,1,0,0};
+        int dc[]={0,0,1,-1};
         for(int m=0;m<4;m++)
         {
             int newx=i+dr[m];
             int newy=j+dc[m];
-            if(newx<0 || newy<0 || newx>=board.size() || newy>=board[0].size())
+            if(newx<0 || newy<0 || newx>=board.size() || newy>=board[0].size() || board[newx][newy]!=word[index+1])
                 continue;
             if(board[newx][newy]==word[index+1])
             {
-               if(helper(newx,newy,board,word,index+1))
-                   return true;
+                if(dfs(board,newx,newy,index+1,word))
+                    return true;
             }
         }
         board[i][j]=ch;
-        
         return false;
     }
 public:
@@ -31,11 +30,14 @@ public:
         {
             for(int j=0;j<board[0].size();j++)
             {
-                if(board[i][j]==word[0]&& helper(i,j,board,word,0))
-                    return true; 
+                if(board[i][j]==word[0] and dfs(board,i,j,0,word))
+                {
+                    return true;
+                }
             }
+
         }
         return false;
-               
+        
     }
 };
