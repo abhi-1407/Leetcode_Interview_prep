172f55fcfdc0fbe05f69e1c5a572ba5f12e82508
abhi-1407 <76861961+abhi-1407@users.noreply.github.com>
2022-12-20 11:45:32 +0530

    Time: 19 ms (52.82%), Space: 10.5 MB (35.11%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -2,30 +2,27 @@ class Solution {
 public:
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         int n=rooms.size();
-        vector<bool> visited(n,false);
+        vector<int> visited(n,false);
+        visited[0]=true;
         queue<int> q;
         q.push(0);
-        for(int i=0;i<rooms[0].size();i++)
-        {
-            q.push(rooms[0][i]);
-        }
         while(!q.empty())
         {
-            auto ele=q.front();
+            int ele=q.front();
             q.pop();
-            visited[ele]=true;
             for(auto it:rooms[ele])
             {
                 if(!visited[it])
-                q.push(it);
+                {
+                    visited[it]=true;
+                    q.push(it);                   
+                }                
             }
         }
-        for(int i=0;i<visited.size();i++)
+        for(auto it:visited)
         {
-            if(visited[i]==false)
-            {
+            if(!it)
                 return false;
-            }
         }
         return true;
     }
