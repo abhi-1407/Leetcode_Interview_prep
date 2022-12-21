7c01b4d4bd5facf87100bd85f37ab326abc7ca25
abhi-1407 <76861961+abhi-1407@users.noreply.github.com>
2022-12-21 12:31:03 +0530

    Time: 26 ms (83.48%), Space: 16.3 MB (91.95%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,27 +1,30 @@
+/* Keep two stacks,s1,is just a normal stack and s2 is for keeping the minimum element,while pushing make sure you push only the elements which are smaller than the top of s2,and while popping see if the element which is being popped in s1 is the top element of the s2 */
+
 class MinStack {
-    multiset<int> s;
-    stack<int> st;  
+    stack<int> s1,s2;
 public:
     MinStack() {
-                     
-    }    
+        
+    }
+    
     void push(int val) {
-        st.push(val);
-        s.insert(val);
+        s1.push(val);
+        if(s2.empty() || val<=getMin())
+            s2.push(val);
     }
     
     void pop() {
-        int ele=st.top();
-        st.pop();
-        s.erase(s.find(ele));
+        if(s1.top()==getMin())
+            s2.pop();
+        s1.pop();
     }
     
     int top() {
-        return st.top();
+        return s1.top();
     }
     
     int getMin() {
-        return *s.begin();
+        return s2.top();
     }
 };
 
