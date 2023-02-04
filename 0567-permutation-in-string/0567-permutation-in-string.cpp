a94d175814ce35e8b480c145d0883877c274ab84
abhi-1407 <76861961+abhi-1407@users.noreply.github.com>
2023-02-04 11:21:29 +0530

    Time: 8 ms (73.53%), Space: 7.4 MB (32.35%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,28 @@
+/*to make it go one back as the while loop breaks after the condition gets unsatisfied,so we need to make it satisfied so i--*/
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if(s2.length()<s1.length())
+            return false;
+        int n=s1.length();
+        vector<int> mpp1(26,0),mpp2(26,0);
+        for(int i=0;i<n;i++)
+        {
+            mpp1[s1[i]-'a']++;
+        }
+        for(int i=0;i<n;i++)
+        {
+            mpp2[s2[i]-'a']++;
+        }
+        for(int i=0;i<s2.length()-s1.length();i++)
+        {
+            if(mpp1==mpp2)
+                return true;
+            mpp2[s2[i]-'a']--;
+            mpp2[s2[i+n]-'a']++;
+        }
+        if(mpp1==mpp2)
+            return true;
+        return false;
+    }
+};
