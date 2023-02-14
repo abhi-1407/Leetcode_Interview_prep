36f82c61ec7d4006ce3fdb5ce47ad8a9c6676e0e
Abhilash Jena <76861961+abhi-1407@users.noreply.github.com>
2023-02-14 11:03:07 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (61.65%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -7,22 +7,30 @@ public:
         if(b.length()==0)
             return a;
         int i=a.length()-1,j=b.length()-1,carry=0;
-        while(i>=0 || j>=0 || carry)
+        while(i>=0 && j>=0)
         {
-            int val=carry;
-            if(i>=0)
-            {
-                val+=(a[i]-'0');
-                i--;
-            }
-            if(j>=0)
-            {
-                val+=(b[j]-'0');
-                j--;
-            }
-            carry=val/2;
-            ans+=((val%2)+'0');
+            int val=carry+(a[i]-'0')+(b[j]-'0');
+            ans.push_back(((val%2)+'0'));
+            carry=(val/2);
+            i--;
+            j--;
         }
+        while(i>=0)
+        {
+            int val=carry+(a[i]-'0');
+            ans.push_back(((val%2)+'0'));
+            carry=(val/2);
+            i--;
+        }
+        while(j>=0)
+        {
+            int val=carry+(b[j]-'0');
+            ans.push_back(((val%2)+'0'));
+            carry=(val/2);
+            j--;
+        }
+        if(carry>0)
+        ans.push_back(carry+'0');
         reverse(ans.begin(),ans.end());
         return ans;
     }
