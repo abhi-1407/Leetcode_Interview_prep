0630d4bcbcd8c00844d68a655557054b4a817842
Abhilash Jena <76861961+abhi-1407@users.noreply.github.com>
2023-03-27 19:38:40 +0530

    Time: 228 ms (61.98%), Space: 106.8 MB (58.49%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
@@ -1,64 +1,33 @@
 class Solution {
-public:
-	// Node stores the index which we have already seen in the cycle
-    int cycle = 0, ans = INT_MIN, node;
-	
-    bool DFS(int i, vector<bool> &visited, vector<bool> &currVisited, vector<vector<int>> &graph){
-        visited[i] = true;
-        currVisited[i] = true;
-		
-		// Traversing through the adjacent vertices
-        for(auto adj: graph[i]){
-			// Not visited
-            if(!visited[adj]){
-				// Cycle found in its adjacent vertices
-                if(DFS(adj, visited, currVisited, graph) && node != i){
-                    cycle++;
-					currVisited[i] = false;
-                    return true;
-                }
-				// Last node in the cycle
-                else{
-					currVisited[i] = false;
-                    return false;
-                }
+    bool helper(int node,vector<int> &edges,vector<int> &visited,vector<int> &path,vector<int> &distance,int d,int &ans)
+    {
+        if(node!=-1)
+        {
+            if(!visited[node])
+            {
+                visited[node]=true;
+                distance[node]=d;
+                path[node]=true;
+                helper(edges[node],edges,visited,path,distance,d+1,ans);
             }
-            // Cycle found
-            else if(visited[adj] && currVisited[adj]){
-                cycle++;
-                node = adj;
-				currVisited[i] = false;
-                return true;
+            else if(path[node])
+            {
+                ans=max(ans,d-distance[node]);
             }
+            path[node]=false;
         }
-        currVisited[i] = false;
-        return false;
+            return false;
     }
-    
+public:
     int longestCycle(vector<int>& edges) {
-		// Create adjacency list from the given edges
-        vector<vector<int>> graph(edges.size());
-        for(int i=0; i<edges.size(); i++){
-            if(edges[i] == -1){
-                continue;
-            }
-            graph[i].push_back(edges[i]);
+        int n=edges.size();
+        vector<int> visited(n,0),path(n,0),distance(n,0);
+        int ans=-1;
+        for(int i=0;i<n;i++)
+        {
+            if(!visited[i])
+                helper(i,edges,visited,path,distance,0,ans);
         }
-        
-        vector<bool> visited(edges.size(), false);
-        vector<bool> currVisited(edges.size(), false);
-        
-        for(int i=0; i<edges.size(); i++){
-            if(!visited[i]){
-                DFS(i, visited, currVisited, graph);
-				// Cycle found
-                if(cycle != 0){
-                    ans = max(ans, cycle+1);
-                    cycle = 0;
-                }
-            }
-        }
-        
-        return ans == INT_MIN ? -1 : ans;
+        return ans;
     }
 };
