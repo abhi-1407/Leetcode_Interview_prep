929117086deab202b03d15feb423a159c846fcba
Abhilash Jena <76861961+abhi-1407@users.noreply.github.com>
2023-04-06 23:09:04 +0530

    Time: 11 ms (84.65%), Space: 9.4 MB (77.35%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,9 +1,8 @@
 class Solution{
-bool dfs(int i,int j,vector<vector<int>>& grid,vector<vector<int>>&vis,vector<int>&delRow,vector<int>&delCol)
+bool dfs(int i,int j,vector<vector<int>>& grid,vector<int>&delRow,vector<int>&delCol)
     {
-       if((i<0 || i>=grid.size() || j<0 || j>=grid[0].size()))
+       if((i<0 || i>grid.size()-1 || j<0 || j>grid[0].size()-1))
            return 0;
-       vis[i][j]=1;
        if(grid[i][j]==1) 
            return 1;    
        grid[i][j]=1;
@@ -12,7 +11,7 @@ bool dfs(int i,int j,vector<vector<int>>& grid,vector<vector<int>>&vis,vector<in
        {
            int newRow=i+delRow[indx];
            int newCol=j+delCol[indx];
-           res=res&(dfs(newRow,newCol,grid,vis,delRow,delCol)); 
+           res=res&(dfs(newRow,newCol,grid,delRow,delCol)); 
        }
        return res;
     }
@@ -24,16 +23,14 @@ bool dfs(int i,int j,vector<vector<int>>& grid,vector<vector<int>>&vis,vector<in
         vector<int>delCol={1,-1,0,0};
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<int>>vis(n,vector<int>(m,0));
         int count=0;
-
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                if(grid[i][j]==0 && vis[i][j]==0)
+                if(grid[i][j]==0)
                 {
-                  if(dfs(i,j,grid,vis,delRow,delCol))
+                  if(dfs(i,j,grid,delRow,delCol))
                     count++;
                 }
             }
