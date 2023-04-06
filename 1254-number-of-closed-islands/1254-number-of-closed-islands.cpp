359d6328a8b2a7dbd920afa742234a2e8be93767
Abhilash Jena <76861961+abhi-1407@users.noreply.github.com>
2023-04-06 23:02:52 +0530

    Time: 21 ms (17.04%), Space: 10.1 MB (24.34%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,43 +1,45 @@
-class Solution {
-    void helper(vector<vector<int>>& grid,int i,int j)
+class Solution{
+bool dfs(int i,int j,vector<vector<int>>& grid,vector<vector<int>>&vis,vector<int>&delRow,vector<int>&delCol)
     {
-        if(i>grid.size()-1 || j>grid[0].size()-1 || i<0 || j<0 || grid[i][j]==1)
-            return;
-        grid[i][j]=1;        
-        int dr[4]={1,-1,0,0};
-        int dc[4]={0,0,1,-1};
-        for(int m=0;m<4;m++)
-        {
-            int newx=dr[m]+i;
-            int newy=dc[m]+j;
-            helper(grid,newx,newy);
-        }
+       if((i<0 || i>=grid.size() || j<0 || j>=grid[0].size()))
+           return 0;
+       vis[i][j]=1;
+       if(grid[i][j]==1) 
+           return 1;    
+       grid[i][j]=1;
+       bool res=true;
+       for(int indx=0;indx<4;indx++)
+       {
+           int newRow=i+delRow[indx];
+           int newCol=j+delCol[indx];
+           res=res&(dfs(newRow,newCol,grid,vis,delRow,delCol)); 
+       }
+       return res;
     }
-public:
+    
+    public:
     int closedIsland(vector<vector<int>>& grid) {
-        vector<vector<int>> visited(grid.size(),vector<int>(grid[0].size(),-1));
+
+        vector<int>delRow={0,0,1,-1};
+        vector<int>delCol={1,-1,0,0};
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>>vis(n,vector<int>(m,0));
         int count=0;
-        for(int i=0;i<grid.size();i++)
+
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<grid[i].size();j++)
+            for(int j=0;j<m;j++)
             {
-                if(grid[i][j]==0 && (i==0 || j==0 ||i==grid.size()-1 || j==grid[0].size()-1))
+                if(grid[i][j]==0 && vis[i][j]==0)
                 {
-                     helper(grid,i,j);
-                }
-            }
-        }
-        for(int i=0;i<grid.size();i++)
-        {
-            for(int j=0;j<grid[i].size();j++)
-            {
-                if(grid[i][j]==0)
-                {
-                     (helper(grid,i,j));
-                        count++;
+                  if(dfs(i,j,grid,vis,delRow,delCol))
+                    count++;
                 }
             }
         }
         return count;
+
+        
     }
 };
