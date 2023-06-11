4973333082ed07152560b7f4685fe8b35b9e99ce
Abhilash Jena <76861961+abhi-1407@users.noreply.github.com>
2023-06-11 20:01:00 +0530

    Time: 0 ms (100.00%), Space: 6 MB (46.27%) - LeetHub

--- 1802-maximum-value-at-a-given-index-in-a-bounded-array/1802-maximum-value-at-a-given-index-in-a-bounded-array.cpp
+++ 1802-maximum-value-at-a-given-index-in-a-bounded-array/1802-maximum-value-at-a-given-index-in-a-bounded-array.cpp
@@ -1,64 +1,30 @@
+#define ll long long int
 class Solution {
 public:
-    long SUM(long n)
-    {
-        return n * (n + 1) / 2;
-    }
-
-    int maxValue(int n, int index, int maxSum)
-    {
-        auto check = [&](int mid)
-        {
-            long sum = 0;
-
-            // sum for 0 to index will be counted below
-            long reqLeft = mid, haveLeft = index + 1;
-            if (index == 0)
-            {
-                sum += mid;
-            }
-            else
-            {
-                if (haveLeft >= reqLeft)
-                {
-                    sum += SUM(mid);
-                    sum += haveLeft - reqLeft;
-                }
-                else
-                {
-                    sum += SUM(mid) - SUM(reqLeft - haveLeft);
-                }
-            }
-
-            // sum for index + 1 to n - 1 will be counted below
-            if (index != n - 1)
-            {
-                long reqRight = reqLeft - 1;
-                long haveRight = n - index - 1;
-
-                if (haveRight >= reqRight)
-                {
-                    sum += SUM(mid - 1);
-                    sum += haveRight - reqRight;
-                }
-                else
-                {
-                    sum += SUM(mid - 1) - SUM(reqRight - haveRight);
-                }
-            }
-
-            return sum <= maxSum;
-        };
-
-        long low = 1, high = maxSum, ans = -1;
-        while (low <= high)
-        {
-            long mid = (low + high) / 2;
-            if (check(mid))
-                ans = mid, low = mid + 1;
-            else
-                high = mid - 1;
+int maxValue(int n, int index, int maxSum) {
+    int emptyR = n - index - 1;
+    int emptyL = index;
+    int h = maxSum, l = 1;
+    ll res = 0;
+    while(l <= h){
+        int mid = (h-l)/2 + l;
+        ll leftSum = 0, rightSum=0, el=mid-1;
+        if(emptyR <= el)
+            rightSum = (el*(el+1))/2 - ((el-emptyR)*(el-emptyR+1))/2;
+        else
+            rightSum = (el*(el+1))/2 + (emptyR - el);
+        if(emptyL <= el)
+            leftSum = (el*(el+1))/2 - ((el-emptyL)*(el-emptyL+1))/2;
+        else
+            leftSum = (el*(el+1))/2 + (emptyL - el);
+        ll sum = leftSum + mid + rightSum;
+        if(sum <= maxSum){
+            l = mid+1;
+            res = mid;
+        }else{
+            h = mid-1;
         }
-        return ans;
+    }
+    return res;
     }
 };
