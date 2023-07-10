8f0f5ef1aeadd07a7490f33d035e2ccfb3f6af0b
Abhilash Jena <76861961+abhi-1407@users.noreply.github.com>
2023-07-10 13:05:54 +0530

    Time: 50 ms (36.45%), Space: 20.8 MB (50.32%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -1,22 +1,21 @@
 class Solution {
-    int helper(vector<int> &satisfaction,int index,int &prev,int time,vector<vector<int>> &dp)
+    int helper(vector<int> &satisfaction,int index,int time,vector<vector<int>> &dp)
     {
-        if(index==satisfaction.size()-1)
+        if(index>=satisfaction.size())
         {
-            return prev+max(0,satisfaction[index])*time;
+           return 0;
         }
         if(dp[index][time]!=-1)
             return dp[index][time];
-        int pick=(satisfaction[index]*time)+helper(satisfaction,index+1,prev,time+1,dp);
-        int notpick=helper(satisfaction,index+1,prev,time,dp);
+        int pick=(satisfaction[index]*time)+helper(satisfaction,index+1,time+1,dp);
+        int notpick=helper(satisfaction,index+1,time,dp);
         return dp[index][time]=max(pick,notpick);
     }
 public:
     int maxSatisfaction(vector<int>& satisfaction) {
         sort(satisfaction.begin(),satisfaction.end());
         int n=satisfaction.size();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
-        int prev=0;
-        return helper(satisfaction,0,prev,1,dp);
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        return helper(satisfaction,0,1,dp);
     }
 };
