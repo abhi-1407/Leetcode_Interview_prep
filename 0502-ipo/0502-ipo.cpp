1dd37b08a867a070c206306a3622b8705e8a35db
Abhilash Jena <76861961+abhi-1407@users.noreply.github.com>
2023-07-25 14:28:26 +0530

    Time: 252 ms (58.55%), Space: 82 MB (46.83%) - LeetHub

--- 0502-ipo/0502-ipo.cpp
+++ 0502-ipo/0502-ipo.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
     int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
-        priority_queue<int> pq;
-        vector<pair<int,int>> temp;
-        for(int i=0;i<profits.size();i++)
+        int n=profits.size();
+        vector<pair<int,int>> vec;
+        for(int i=0;i<n;i++)
         {
-            temp.push_back({capital[i],profits[i]});
+            vec.push_back({capital[i],profits[i]});
         }
-        sort(temp.begin(),temp.end());
-        int total=0,index=0;
+        sort(vec.begin(),vec.end());
+        int ind=0;
+        priority_queue<int> pq;
         while(k)
         {
-            while(index<profits.size() && temp[index].first<=w)
+            while(ind<n && vec[ind].first<=w)
             {
-                pq.push(temp[index].second);
-                index++;
+                pq.push(vec[ind].second);
+                ind++;
             }
-            if(pq.empty())
-                break;
-            w+=pq.top();
-            pq.pop();
-            k=k-1;
+            if(!pq.empty())
+            {
+                w+=pq.top();
+                pq.pop();
+            }
+            k--;
         }
-        return w;       
+        return w;
+              
     }
 };
