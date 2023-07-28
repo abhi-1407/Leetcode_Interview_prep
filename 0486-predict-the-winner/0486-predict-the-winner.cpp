e377a5d5efa94e12c2b4b5981dc051bd000ebda9
Abhilash Jena <76861961+abhi-1407@users.noreply.github.com>
2023-07-28 21:31:12 +0530

    Time: 9 ms (40.95%), Space: 8.6 MB (8.42%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,5 +1,5 @@
 class Solution {
-    bool helper(int i,int j,vector<int> &nums,int s1,int s2,bool turn)
+    bool helper(int i,int j,vector<int> &nums,int s1,int s2,bool turn,vector<vector<vector<int>>> &dp)
     {
         if(i>j)
         {
@@ -8,16 +8,17 @@ class Solution {
         bool ans=false;
         if(turn)
         {
-            ans=(helper(i+1,j,nums,s1+nums[i],s2,!turn)||helper(i,j-1,nums,s1+nums[j],s2,!turn));
+            ans=(helper(i+1,j,nums,s1+nums[i],s2,!turn,dp)||helper(i,j-1,nums,s1+nums[j],s2,!turn,dp));
         }
         else
         {
-           ans=(helper(i+1,j,nums,s1,s2+nums[i],!turn)&&helper(i,j-1,nums,s1,s2+nums[j],!turn));     
+           ans=(helper(i+1,j,nums,s1,s2+nums[i],!turn,dp)&&helper(i,j-1,nums,s1,s2+nums[j],!turn,dp));     
         }
         return ans;
     }
 public:
     bool PredictTheWinner(vector<int>& nums) {
-        return helper(0,nums.size()-1,nums,0,0,true);
+        vector<vector<vector<int>>> dp(nums.size(),vector<vector<int>>(nums.size(),vector<int>(2,-1)));
+        return helper(0,nums.size()-1,nums,0,0,true,dp);
     }
 };
