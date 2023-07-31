c3763fc9acccf03821b7ed844649a9ca50dfb924
Abhilash Jena <76861961+abhi-1407@users.noreply.github.com>
2023-07-31 10:01:31 +0530

    Time: 43 ms (72.41%), Space: 15.4 MB (14.89%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,31 +1,46 @@
 class Solution {
-public:
-    int minimumDeleteSum(string s1, string s2) {
-        int n1 = s1.length(), n2 = s2.length();
-        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, 0));
-
-        for (int i = 1; i <= n1; i++) {
-            for (int j = 1; j <= n2; j++) {
-                if (s1[i - 1] == s2[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1] + (int)s1[i - 1];
-                } else {
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-                }
+    int helper(int index1,int index2,string &s1,string &s2,vector<vector<int>> &dp)
+    {
+        if(index1<0 && index2<0)
+        {
+            return 0;
+        }
+        else if(index1<0)
+        {
+            int sum=0;
+            for(int i=index2;i>=0;i--)
+            {
+                sum+=(int)s2[i];
             }
+            return sum;
         }
-
-        int lcsSum = dp[n1][n2];
-        int s1Sum = 0;
-        int s2Sum = 0;
-
-        for (char c : s1) {
-            s1Sum += (int)c;
+        else if(index2<0)
+        {
+            int sum=0;
+            for(int i=index1;i>=0;i--)
+            {
+                sum+=(int)s1[i];
+            }
+            return sum;
         }
-
-        for (char c : s2) {
-            s2Sum += (int)c;
+        if(dp[index1][index2]!=-1)
+            return dp[index1][index2];
+        int ans=0;
+        if(s1[index1]==s2[index2])
+        {
+            ans=helper(index1-1,index2-1,s1,s2,dp);
         }
+        else
+        {
+            ans=min((int)s1[index1]+helper(index1-1,index2,s1,s2,dp),(int)s2[index2]+helper(index1,index2-1,s1,s2,dp));
+        }
+        return dp[index1][index2]=ans;
+    }
 
-        return s1Sum + s2Sum - 2 * lcsSum;
+    public:
+    int minimumDeleteSum(string s1, string s2) {
+        int n=s1.length(),m=s2.length();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        return helper(n-1,m-1,s1,s2,dp);
     }
 };
