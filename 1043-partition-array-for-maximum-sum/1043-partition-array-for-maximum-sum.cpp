9578ef0626df2b41cd116ce5b2aaedbd07781d5a
Abhilash Jena <76861961+abhi-1407@users.noreply.github.com>
2024-02-03 18:23:42 +0530

    Time: 12 ms (68.61%), Space: 11.1 MB (8.52%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -5,11 +5,11 @@ class Solution {
             return 0;
         if(dp[index]!=-1)
             return dp[index];
-        int maxi=0,maxAns=INT_MIN;
+        int maxi=INT_MIN,maxAns=0;
         for(int length=0;length<k && length+index<arr.size();length++)
         {
             maxi=max(maxi,arr[length+index]);
-            int sum=maxi*(length+1)+helper(arr,index+length+1,k,dp);//length+1 because 0 length is actually 1
+            int sum=maxi*(length+1)+helper(arr,index+length+1,k,dp);
             maxAns=max(maxAns,sum);
         }
         return dp[index]=maxAns;
