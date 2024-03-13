272661196a08ccc9e8609e36f6786fb4e055e681
Abhilash Jena <76861961+abhi-1407@users.noreply.github.com>
2024-03-14 00:54:21 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (41.86%) - LeetHub

---
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int pivotInteger(int n) {
+        int sum=(n*(n+1))/2;
+        int curr=0;
+        for(int i=1;i<=n;i++)
+        {
+            curr+=i;
+            if(curr==sum+i-curr)
+                return i;
+        }
+        return -1;
+    }
+};
+
+
+    
