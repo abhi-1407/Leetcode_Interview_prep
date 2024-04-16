6109980c914c0e36da95f4d8d93ffad87b1c9c99
Abhilash Jena <76861961+abhi-1407@users.noreply.github.com>
2024-04-17 01:34:32 +0530

    Time: 8 ms (91.46%), Space: 24.3 MB (74.27%) - LeetHub

---
+++ 0623-add-one-row-to-tree/0623-add-one-row-to-tree.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    void helper(TreeNode* root,int height,int val,int depth)
+    {
+        if(height+1==depth)
+        {
+            TreeNode *node1=new TreeNode(val);
+            TreeNode *node2=new TreeNode(val);
+            node1->left=root->left;
+            node2->right=root->right;
+            root->left=node1;
+            root->right=node2;
+            return;
+        }
+        if(root->left)
+        helper(root->left,height+1,val,depth);
+        if(root->right)
+        helper(root->right,height+1,val,depth);
+    }
+public:
+    TreeNode* addOneRow(TreeNode* root, int val, int depth) {
+        if(depth==1)
+        {
+            TreeNode *node=new TreeNode(val);
+            node->left=root;
+            return node;
+        }
+        int height=1;
+        helper(root,height,val,depth);
+        return root;
+    }
+};
