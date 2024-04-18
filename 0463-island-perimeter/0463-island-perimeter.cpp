90834d866fad4865fb64a4af6abd5a6513dfd41b
Abhilash Jena <76861961+abhi-1407@users.noreply.github.com>
2024-04-18 23:54:21 +0530

    Time: 84 ms (28.86%), Space: 100.7 MB (94.99%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -11,9 +11,11 @@ public:
                         count ++;
                         if(i!=0 && grid[i-1][j] == 1) repeat++;
                         if(j!=0 && grid[i][j-1] == 1) repeat++;
+                        if(i+1<grid.size() && grid[i+1][j] == 1) repeat++;
+                        if(j+1<grid[0].size() && grid[i][j+1] == 1) repeat++;
                     }
                 }
         }
-        return 4*count-repeat*2;
+        return 4*count-repeat;
     }
 };
