1643cb6f22d2c95388efe5522b31e39f881e6b25
Abhilash Jena <76861961+abhi-1407@users.noreply.github.com>
2024-06-26 00:54:29 +0530

    Time: 5 ms (15.49%), Space: 10.1 MB (42.71%) - LeetHub

--- 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.cpp
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.cpp
@@ -10,43 +10,7 @@
  * };
  */
 class Solution {
-//     void helper(TreeNode *root,vector<int> &ans)
-//     {
-//         if(root==NULL)
-//             return;
-//         helper(root->left,ans);
-//         ans.push_back(root->val);
-//         helper(root->right,ans);
-//     }
-    
-//     void builder(TreeNode *root,vector<int> &prefix,map<int,int> &mpp)
-//     {
-//         if(root==NULL)
-//             return;
-//         builder(root->left,prefix,mpp);
-//         root->val=mpp[root->val];
-//         builder(root->right,prefix,mpp);
-//     }
-// public:
-//     TreeNode* bstToGst(TreeNode* root) {
-//         if(root==NULL)
-//             return root;
-//         vector<int> ans,prefix;
-//         helper(root,ans);
-//         prefix.resize(ans.size());
-//         prefix[ans.size()-1]=ans[ans.size()-1];
-//         for(int i=ans.size()-2;i>=0;i--)
-//             prefix[i]=prefix[i+1]+ans[i];
-//         map<int,int> mpp;
-//         for(int i=0; i<ans.size(); i++){
-//             mpp[ans[i]] = prefix[i];
-//         }
-//         builder(root,prefix,mpp);
-//         return root;
-//     }
-    
-    
-    void helper(TreeNode *root,int &sum)
+void helper(TreeNode *root,int &sum)
     {
        if(root==NULL)
          return;
